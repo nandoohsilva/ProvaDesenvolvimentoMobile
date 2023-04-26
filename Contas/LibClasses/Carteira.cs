b04a3373f4c69731049a7ece94202490d8934e02
00099299 <00099299@CIDLAB108M4>
2023-04-25 21:23:34 -0300

    Adicionar arquivos de projeto.

---
+++ Contas/LibClasses/Carteira.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contas.LibClasses
+{
+    public class Carteira
+    {
+        public double Saldo
+        {
+            get;
+            private set;
+        }
+        public string Dono { get; set; }
+
+        public string Cpf
+        {
+            get;
+            set;
+        }// adciona um cpf a conta
+        public double LimiteConta // adicionando o campo LimiteConta
+        {
+            get;
+            set;
+        }
+        public DateTime Tarifa // adicionando o campo LimiteConta
+        {
+            get;
+            set;
+        }
+        public bool cobrarTarifa(DateTime dataTarifa)
+        {
+            this.Saldo -= 19.90;
+            this.Tarifa = dataTarifa;
+            return true;
+        }
+
+        public bool Sacar(double Valor)
+        {
+            if (Valor > this.Saldo)
+                return false;
+
+            this.Saldo -= Valor;
+            //this.Saldo = Saldo - Valor;
+            return true;
+        }
+
+        public bool Depositar(double Valor)
+        {
+            this.Saldo += Valor;
+            return true;
+        }
+
+        public bool Transferir
+            (Carteira destino, double valor)
+        {  
+            //se nao tiver saldo cancela transferencia retornando false
+            if (this.Saldo <= valor)
+                return false;
+
+            //Executa transferencia tirando da conta origram e deposinto na conta destino
+            this.Sacar(valor);
+            bool tOK = destino.Depositar(valor);
+            if (tOK)// se transferencia ocorreu com sucesso retorna true
+                return true;
+            else// caso ocorrer erro faz o rollback voltando dinheiro para conta de origem
+            {
+                this.Depositar(valor);
+                return false;
+            }
+        }
+    }
+}
---
+++ Contas/LibClasses/DadosCarteiras.cs
@@ -0,0 +1,10 @@
+﻿namespace Contas.LibClasses
+{
+    public class DadosCarteiras
+    {
+        public List<Carteira> ListaDados = new List<Carteira>();
+        public DateTime DataDoSistema { get; set; } = DateTime.Now;
+
+      
+    }
+}
